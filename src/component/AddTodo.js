fb34311602e0c213379c4a65362554d7c3720c0b
SkyeJackson <skyejackson@spelman.edu>
2022-06-22 19:50:55 -0400

    added due date feature

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
-
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
   constructor() {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due:null
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
@@ -33,6 +35,11 @@ class AddTodo extends Component {
       });
     }
   };
+  datePicker = (event) => {
+    this.setState({
+      date:newDate(event).toLocaleDateString()
+    });
+  };
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
@@ -49,6 +56,17 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
+
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+          <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.due}
+            onChange={datePicker()}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
